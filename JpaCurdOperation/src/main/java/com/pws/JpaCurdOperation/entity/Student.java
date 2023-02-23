731fb7a31352f00dae02a14cf49f3c75b564c963
Raghavendra-Mahendrakar <114050900+Raghavendra-Mahendrakar@users.noreply.github.com>
2023-02-23 22:52:13 +0530

    Swagger_OpenAPI

---
+++ JpaCurdOperation/src/main/java/com/pws/JpaCurdOperation/entity/Student.java
@@ -0,0 +1,25 @@
+package com.pws.JpaCurdOperation.entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+    private String branch;
+}
